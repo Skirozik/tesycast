e72a74d35b46e347a45efb438735e417e3f477d1
Simeon Chere <simeonchere@gmail.com>
2026-02-23 22:18:03 -0500

    Initial Commit

---
+++ TeslaStream2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TeslaStream2
+//
+//  Created by Simeon Chere on 2/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TeslaStream2/TeslaStream2App.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaStream2App.swift
+//  TeslaStream2
+//
+//  Created by Simeon Chere on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct TeslaStream2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
